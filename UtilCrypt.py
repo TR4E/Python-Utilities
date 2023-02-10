720a714520f6136b859a44087415af1db017458c
TR4E <trae.rohleder@outlook.com>
2023-02-10 17:37:09 +1100

    Init

--- UtilCrypt.py
+++ UtilCrypt.py
@@ -5,8 +5,6 @@ def hash(string, amount=1, pepper=None):
     if pepper is not None:
         string = pepper + "$" + string
 
-    print(string)
-
     for i in range(amount):
         string = hashlib.sha512(bytes(string, "UTF-8")).hexdigest()
 
