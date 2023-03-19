efe3127bd79be34f0d4fac11fbf5d12378915671
TR4E <trae.rohleder@outlook.com>
2023-03-20 00:32:10 +1100

    Changes

--- UtilCrypt.py
+++ UtilCrypt.py
@@ -1,42 +1,34 @@
 import base64
-import hashlib
 
-from Crypto.Cipher import AES
+from cryptography.fernet import Fernet
 
 
-def hash(string, amount=1, pepper=None, type=hashlib.sha512):
-    if pepper is not None:
-        string = pepper + "$" + string
+def generate_key(key):
+    result = key
+
+    while len(result) < 32:
+        result += "X"
+
+    return base64.urlsafe_b64encode(result.encode())
+
+
+def encrypt(key, string, amount):
+    result = string
 
     for i in range(amount):
-        string = hashlib.sha512(bytes(string, "UTF-8")).hexdigest()
+        cipher = Fernet(generate_key("{}:{}".format(amount, key)))
 
-    return string
+        result = cipher.encrypt(result.encode()).decode("UTF-8")
+
+    return result
 
 
-class AES256:
+def decrypt(key, string, amount):
+    result = string
 
-    def __init__(self, key):
-        self.key = hashlib.sha256(key.encode()).digest()
-        self.iv = b'\r\xe8\xf2\xaa\x82\x07\x81\xad\xd9\x0e\xfaZ?\xa5wT'
+    for i in range(amount):
+        cipher = Fernet(generate_key("{}:{}".format(amount, key)))
 
-    def encrypt(self, plaintext):
-        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        result = cipher.decrypt(result).decode("UTF-8")
 
-        padding_length = 16 - (len(plaintext) % 16)
-        plaintext += chr(padding_length) * padding_length
-
-        ciphertext = cipher.encrypt(plaintext.encode())
-
-        return base64.b64encode(self.iv + ciphertext)
-
-    def decrypt(self, ciphertext):
-        ciphertext = base64.b64decode(ciphertext)
-
-        iv = ciphertext[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-
-        plaintext = cipher.decrypt(ciphertext[16:]).decode()
-        padding_length = ord(plaintext[-1])
-
-        return plaintext[:-padding_length]
+    return result
